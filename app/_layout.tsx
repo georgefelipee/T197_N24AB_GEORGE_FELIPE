fe7dae252fb4c0801437d42cf6d13482666d69f0
MayraLeticia <mayraleticia03@gmail>
2025-04-09 19:55:51 -0300

    feat: login page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,15 @@
 import { Stack } from "expo-router";
+import * as React from 'react';
+import { AppRegistry } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
 
 export default function RootLayout() {
-  return <Stack  screenOptions={{ headerShown: false }}>
-    <Stack.Screen name="login" options={{ headerShown: false }}  />
-    <Stack.Screen name="home" options={{ headerShown: false }} />
-  </Stack>;
+  return (
+  <PaperProvider>
+    <Stack initialRouteName="login" screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="login" options={{ headerShown: false }}  />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>;
+  </PaperProvider>
+  )
 }
