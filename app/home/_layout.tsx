be4d2a3af0c14c44a0bd4d17b3b153a76cef6199
Igor Mapurunga <mapurungaigor@gmail.com>
2025-05-12 17:36:33 -0300

    modificado os icones do footer

--- app/home/_layout.tsx
+++ app/home/_layout.tsx
@@ -1,14 +1,41 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Tabs } from 'expo-router'
+import React from 'react';
+import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function HomeLayout() {
   return (
     <>
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="HomeDocuments" options={{headerShown: false }} />
-      <Tabs.Screen name= "Perfil" options={{headerShown: false }} />
-    </Tabs>
+      <Tabs
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => {
+            let iconName: keyof typeof Ionicons.glyphMap;
+
+            if (route.name === 'HomeDocuments') {
+              iconName = 'home-outline'; // Ícone de casa
+            } else if (route.name === 'Perfil') {
+              iconName = 'person-outline'; // Ícone de perfil
+            } else {
+              iconName = 'ellipse-outline'; // fallback
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tabs.Screen
+          name="HomeDocuments"
+          options={{
+            tabBarLabel: 'Home', // Nome exibido
+          }}
+        />
+        <Tabs.Screen
+          name="Perfil"
+          options={{
+            tabBarLabel: 'Perfil',
+          }}
+        />
+      </Tabs>
     </>
-  )
+  );
 }
